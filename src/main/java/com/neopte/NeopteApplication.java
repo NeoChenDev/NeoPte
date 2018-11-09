24daa9ca967b2d98d308ed053910f0fa51bc2d7e
neo <neo_chen@aliyun.com>
2018-11-09 15:44:59 +0800

    springboot项目搭建

---
+++ src/main/java/com/neopte/NeopteApplication.java
@@ -0,0 +1,12 @@
+package com.neopte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeopteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeopteApplication.class, args);
+    }
+}
